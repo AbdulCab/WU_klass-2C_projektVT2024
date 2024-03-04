aaf8d85712fe87350fbb2f8673494ce1d8af2522
Abdulmannan Cab <151527636+AbdulCab@users.noreply.github.com>
2024-03-04 14:31:23 +0100

    bop

--- js/script.js
+++ js/script.js
@@ -39,4 +39,30 @@ document.getElementById('arrow_left').addEventListener('click', newImageLeft)
 
 document.getElementById('arrow_right').addEventListener('click', newImageRight)
 
+var touchstartX = 0;
+var touchendX = 0;
+var touchableElement = document.getElementById('gallery_img');
+
+touchableElement.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+touchableElement.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        newImageRight()
+    }
+
+    if (touchendX > touchstartX) {
+        newImageLeft()
+    }
+}
+
 
